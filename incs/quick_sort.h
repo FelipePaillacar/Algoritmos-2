b09133c681612030c993642d249d424e0224e6d9
Felipe Paillacar <139182877+FelipePaillacar@users.noreply.github.com>
2026-05-13 00:09:28 -0300

    Delete incs directory

--- incs/quick_sort.h
+++
@@ -1,8 +0,0 @@
-#ifndef QUICK_SORT_H
-#define QUICK_SORT_H
-
-#include "deportista.h"
-
-void quickSort(Deportista arr[], int low, int high);
-
-#endif
--- incs/utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-
-#include "deportista.h"
-
-void swap(Deportista *a, Deportista *b);
-
-#endif
